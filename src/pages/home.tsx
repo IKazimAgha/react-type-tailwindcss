31bf215f33bfb2bc0e86b968c261573311e40a45
Agha Ali Kazim <kazim.agha@systemsltd.com>
2025-02-11 17:38:21 +0500

    routing home page

--- src/pages/home.tsx
+++ src/pages/home.tsx
@@ -1,11 +1,22 @@
 import React from 'react';
 import { Button } from '../components/button';
+import KidSVG from "../icons/kid.svg"
+import SalamaSVG from "../icons/salama.svg";
 
 const Home = () => {
     return(
-        <div>
-            Hello world
-            <Button />
+        <div >
+            <div className="pt-10 border-b-2 border-blue-500 rounded-lg bg-yellow-500 border-b-2 border-blue-500 rounded-b-[5rem]">
+                <div className="">
+                    <div className="flex justify-center">
+                        <img src={SalamaSVG} />
+                    </div>
+                    <div>Peace of Mind, Every Ride!</div>
+                    <div>
+                        <img src={KidSVG} />
+                    </div>
+                </div>
+            </div>
         </div>
     )
 }
